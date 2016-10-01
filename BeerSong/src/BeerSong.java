4b6d595cf9f6d8f11be78ff7eef7df9e78bbfce2
Timothy Rothe” <trrothe@oakland.edu>
2016-09-30 21:18:50 -0400

    Initial implementation with functionless (stubbed) methods.

---
+++ BeerSong/src/BeerSong.java
@@ -0,0 +1,79 @@
+/*
+ *******************************************************************************
+ *
+ *     STUDENT: Timothy Rothe (trrothe@oakland.edu)
+ *  ASSIGNMENT: Assignment #2, Part II (GIT-BeerSong) 
+ *       CLASS: CSE337, 2016-Fall, MW/1530-1717/DH200, Prof. Nilesh Patel
+ *  UNIVERSITY: Oakland University
+ *    CREATION: 30-Sep-2016 (Friday)
+ *         DUE: 30-Sep-2016@2355est (Friday)
+ *  SUBMISSION: --pending--
+ *
+ *  (c) Copyright 2016 Timothy R.H. Rothe. All rights reserved. 
+ * 
+ *******************************************************************************
+ * CHANGE LOG: 
+ * 
+ * Change #0, MadeOn:2016-Sep-30@1039, MadeBy:trrothe@oakland.edu
+ * VERSION: 1.0.0.0
+ * DESCRIPTION:
+ *     1) Initial implementation. 
+ *	   2) Implements stubbed methods (functions), i.e., functionsless methods.  
+ *        That is, 
+ *            public class BeerSong is empty
+ *            public static void main(String[] args) is empty
+ * 
+ *******************************************************************************
+ */
+package BeerSong;
+
+/*
+********************************************************************************
+* public class BeerSong
+********************************************************************************
+*/
+public class BeerSong 
+    {
+    /**
+     * @param args the command line arguments
+     */
+
+
+    
+    /*
+    ****************************************************************************
+    * public static void Ninety_Nine_Bottles_of_Beer()
+    ****************************************************************************
+    */
+    public static void Ninety_Nine_Bottles_of_Beer() 
+        {
+        }
+        /*
+        ************************************************************************
+        * end public static void Ninety_Nine_Bottles_of_Beer()
+        ************************************************************************
+        */
+    
+    
+    
+    /*
+    ****************************************************************************
+    * public static void main(String[] args)
+    ****************************************************************************
+    */
+    public static void main(String[] args) 
+        {
+        }
+        /*
+        ************************************************************************
+        * end public static void main(String[] args)
+        ************************************************************************
+        */
+
+    }
+    /*
+    ****************************************************************************
+    * public class BeerSong
+    ****************************************************************************
+    */
+
